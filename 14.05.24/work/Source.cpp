f36fa05de8209fb6920464be01ad622f7078592f
Nik <thezagnik@gmail.com>
2024-05-16 21:00:00 +0400

    дописал некоторые методы

--- 14.05.24/work/Source.cpp
+++ 14.05.24/work/Source.cpp
@@ -66,6 +66,16 @@ public:
 		return size == 0;
 	}
 
+	void Clear()
+	{
+		if (arr != nullptr)
+		{
+			delete[] arr;
+			this->size = 0;
+			arr = nullptr;
+		}
+	}
+
 	void Resize(int size)
 	{
 		int* newArr = new int[size];
@@ -104,6 +114,91 @@ public:
 			sort(arr, arr + size, [](int& a, int& b) { return a < b; });
 	}
 
+	void Swap(int firstIndex, int secondIndex)
+	{
+		swap(arr[firstIndex], arr[secondIndex]);
+	}
+
+	void Concatinate(const Array& other)
+	{
+		int newSize = other.size + this->size;
+		int* newArr = new int[newSize];
+		for (int i = 0; i < this->size; i++)
+			newArr[i] = this->arr[i];
+		for (int i = 0; i < newSize; i++)
+			newArr[size + i] = other.arr[i];
+
+		if (arr != nullptr)
+			delete[] arr;
+
+		this->size = newSize;
+		arr = newArr;
+	}
+
+	void Insert(int index, int value)
+	{
+		int newSize = size + 1;
+		int* newArr = new int[newSize];
+		
+		for (int i = 0; i < index; i++)
+			newArr[i] = arr[i];
+
+		newArr[index] = value;
+
+		for(int i = index; i < size; i++)
+			newArr[i + 1] = arr[i];
+
+		if (arr != nullptr)
+			delete[] arr;
+
+		this->size = newSize;
+		arr = newArr;
+	}
+
+	void InsertFront(int value)
+	{
+		Insert(0, value);
+	}
+	
+	void InsertBack(int value)
+	{
+		Insert(size, value);
+	}
+
+	void Remove(int index)
+	{
+		int newSize = size - 1;
+		int* newArr = new int[newSize];
+		for (int i = 0; i < index; i++)
+			newArr[i] = arr[i];
+
+		for (int i = index; i < newSize; i++)
+			newArr[i] = arr[i + 1];
+
+		if (arr != nullptr)
+			delete[] arr;
+
+		size = newSize;
+		arr = newArr;
+	}
+
+	void RemoveFront()
+	{
+		Remove(0);
+	}
+
+	void RemoveBack()
+	{
+		Remove(size);
+	}
+
+	void Replace(int oldValue, int newValue)
+	{
+		for (int i = 0; i < size; i++)
+			if (arr[i] == oldValue)
+				arr[i] = newValue;
+	}
+
 	int GetAverage() 
 	{
 		if (IsEmpty())
@@ -114,6 +209,16 @@ public:
 			sum += arr[i];
 
 		return (double)sum / size;
+	}
+
+	int GetSum() const
+	{
+		int sum = 0;
+		
+		for (int i = 0; i < size; i++)
+			sum += arr[i];
+
+		return sum;
 	}
 
 	int GetMin() 
@@ -140,6 +245,22 @@ public:
 				max = arr[i];
 
 		return max;
+	}
+
+	int Find(int value) // return index or -1
+	{
+		for (int i = 0; i < size; i++)
+			if (arr[i] == value)
+				return i;
+		return -1;
+	}
+
+	int FindLast(int value) // return index or -1
+	{
+		for (int i = size; i > 0; i--)
+			if (arr[i] == value)
+				return i;
+		return -1;
 	}
 
 	string ToString() 
@@ -169,6 +290,8 @@ void main()
 	Array arr3(3, 999);
 	Array arr4(temp, size(temp));
 	Array arr5 = arr4;
+	Array arr6(3, 999);
+
 
 	cout << "arr1 = [" << arr1.ToString() << "] with size = " << arr1.Size() << endl;
 	cout << "arr2 = [" << arr2.ToString() << "] with size = " << arr2.Size() << endl;
@@ -186,6 +309,51 @@ void main()
 	cout << "arr1 is empty = " << arr1.IsEmpty() << endl;
 	cout << "arr2 is empty = " << arr2.IsEmpty() << endl;
 	cout << "arr3 is empty = " << arr3.IsEmpty() << endl;
+
+	cout << endl << "Is concatinate test" << endl;
+
+	cout << "arr1 + arr2 = ";
+	arr2.Concatinate(arr3);
+	arr2.ToString();
+
+	cout << endl << "Is clear test" << endl;
+	arr6.Clear();
+	cout << "arr6 = [" << arr6.ToString() << "] with size = " << arr6.Size() << endl;
+
+	cout << endl << "Is swap test" << endl;
+	arr4.Swap(1, 2);
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is insert test" << endl;
+	arr4.Insert(1, 6);
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is insert front test" << endl;
+	arr4.InsertFront(9);
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is insert back test" << endl;
+	arr4.InsertBack(9);
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is remove test" << endl;
+	arr4.Remove(1);
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is remove front test" << endl;
+	arr4.RemoveFront();
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is remove back test" << endl;
+	arr4.RemoveBack();
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is replace back test" << endl;
+	arr4.Replace(6, 8);
+	cout << "arr4 = [" << arr4.ToString() << "] with size = " << arr4.Size() << endl;
+
+	cout << endl << "Is sum back test" << endl;
+	cout << "arr4 sum = " << arr4.GetSum() << endl;
 
 	cout << endl << "Resize test" << endl;
 
@@ -217,3 +385,4 @@ void main()
 
 	system("pause");
 }
+
