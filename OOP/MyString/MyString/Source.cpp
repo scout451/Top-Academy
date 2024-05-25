195d30a3c68a9bd84d7baa4eb7775e667d277c88
NikClick <thezagnik@gmail.com>
2024-05-25 14:57:20 +0400

    дописал конструкторы копирования и перемещения, а также перегрузку их операторов

--- OOP/MyString/MyString/Source.cpp
+++ OOP/MyString/MyString/Source.cpp
@@ -2,57 +2,79 @@
 
 using namespace std;
 
-class MyString
+class MyString 
 {
     char* str;
 
 public:
+    MyString() : MyString(80) {}
 
-    MyString() : str(new char[80]) {} // Конструктор по умолчанию
+    MyString(int size) : str(new char[size]) {}
 
-    MyString(int size) : str(new char[size])
-    {}
-
-    MyString(const char* newStr)
+    MyString(const char* newStr) 
     {
-        if (str != nullptr)
-        {
-            delete[] str;
-            str = nullptr;
-        }
-
-        // Выделяем память для новой строки
-        str = new char[strlen(newStr) + 1]; // +1 для нуль-терминатора
+        str = new char[strlen(newStr) + 1];
         strcpy_s(str, strlen(newStr) + 1, newStr);
     }
 
     ~MyString() 
     {
-        if (str != nullptr) 
-            delete[] str; 
-    } // Деструктор для освобождения памяти
+        delete[] str;
+    }
 
-    const char* Get() { return str; }
-    void Set(const char* newStr)
+    MyString(const MyString& other) {
+        str = new char[strlen(other.str) + 1];
+        strcpy_s(str, strlen(other.str) + 1, other.str);
+    }
+
+    MyString& operator=(const MyString& other) {
+        if (this == &other) return *this;
+
+        delete[] str;
+        str = new char[strlen(other.str) + 1];
+        strcpy_s(str, strlen(other.str) + 1, other.str);
+        return *this;
+    }
+
+    MyString(MyString&& other)
     {
-        // Удаляем старую строку, если она есть
-        if (str != nullptr)
-        {
-            delete[] str;
-            str = nullptr;
-        }
+        str = other.str;
+        other.str = nullptr;
+    }
 
-        // Выделяем память для новой строки
+    MyString& operator=(MyString&& other)
+    {
+        if (this == &other)
+            return *this; 
+
+        delete[] str;
+        str = other.str;
+        other.str = nullptr;
+        return *this;
+    }
+    
+    const char* Get() const { return str; }
+
+    void Set(const char* newStr) 
+    {
+        delete[] str;
         str = new char[strlen(newStr) + 1];
         strcpy_s(str, strlen(newStr) + 1, newStr);
     }
 };
 
-int main()
+void main() 
 {
     MyString str;
     str.Set("priva");
     cout << str.Get() << endl;
 
-    return 0;
+    MyString str2 = str; 
+    cout << str2.Get() << endl;
+
+    MyString str3;
+    str3 = move(str2);
+    cout << str3.Get() << endl;
+
+    system("pause");
 }
